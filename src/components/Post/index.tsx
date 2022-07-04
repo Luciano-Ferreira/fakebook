604b99c000d7a43978017f50abe2f03cf6cdc812
lfs <luciano2023silva@gmail.com>
2022-07-04 19:37:25 -0300

    task: post scss module

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,34 +1,50 @@
+import styles from './Post.module.scss';
+
 interface Props {
   avatar: string;
   author: string;
   bio: string;
-  content: string;
   publishedAt: string;
 }
 
-export function Post({ author, bio, avatar, publishedAt, content }: Props) {
+export function Post({ author, bio, avatar, publishedAt }: Props): JSX.Element {
   return (
-    <article className='flex flex-col'>
-      <header className='flex'>
-        <div className='flex'>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
           <img
-            className='h-16 w-16 rounded-full border-2 border-green-500'
             src={avatar}
             alt={author}
           />
-          <div className='flex flex-col'>
-            <h3>{author}</h3>
-            <p>{bio}</p>
+          <div className={styles.authorInfo}>
+            <strong>{author}</strong>
+            <span>{bio}</span>
           </div>
         </div>
-        <time 
+        <time
           title='11 de maio às 08:13'
           dateTime='2022-05-11 08:13:30'
         >{publishedAt}</time>
       </header>
-      <div className=''>
-        <p>{content}</p>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>
+          <a href="#">jane.design/doctorcare</a>{' '}
+          <a href="#">#novoprojeto</a> {' '}
+          <a href="#">#nlw</a> {' '}
+          <a href="#">#rocketseat</a>
+        </p>
       </div>
+      <form
+        className={styles.commentForm}
+      >
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+        <button type='submit'>Comentar</button>
+      </form>
     </article>
   )
 }
